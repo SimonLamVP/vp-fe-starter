9fb5dd3149bf26c5f1b6b66aca00ab4bef2f359c
Simon Lam <simon.lam@victorianplumbing.co.uk>
2026-03-19 19:45:40 +0000

    themeing

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,15 @@ import { Products } from "./features/products/Products"
 export const App = () => (
   <div className="App">
     <section className="App-panel">
+      <div className="App-titleBar">
+        <span>Carts Explorer</span>
+      </div>
       <Carts />
     </section>
     <section className="App-panel">
+      <div className="App-titleBar">
+        <span>Products Explorer</span>
+      </div>
       <Products />
     </section>
   </div>
